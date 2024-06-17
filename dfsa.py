f7e278fa87967c19b326b8bd8331e20a8d81d0ba
haldey <haldey86@mail.ru>
2024-06-17 22:44:43 +0300

    testovoe fullstek

---
+++ dfsa.py
@@ -0,0 +1,18 @@
+passwords = [123456, 23456, 8569]
+for i in passwords:
+    
+    logins = ['wlad', 'egor' ]
+
+def passw(passwords):
+    for i in passwords:
+        return i
+
+passw(passwords)
+password = 1233456
+if password == passw:
+    print('r')
+
+
+login = 'wlad'
+if login == logins:
+    print('of')
